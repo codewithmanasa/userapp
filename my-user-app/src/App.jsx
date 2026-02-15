18b25b499ca9b65418432067e2d24847b78106ac
CodeBase <149752706+codewithmanasa@users.noreply.github.com>
2026-02-15 21:19:41 +0530

    Update App.jsx

--- my-user-app/src/App.jsx
+++ my-user-app/src/App.jsx
@@ -5,7 +5,7 @@ import {
   Route,
   Navigate,
 } from "react-router-dom";
-import { AuthProvider } from "./context/Authcontext.jsx";
+import { AuthProvider } from "./context/AuthContext.jsx";
 import PrivateRoute from "./components/ProtectedRoute.jsx";
 import Login from "./pages/Login";
 import Register from "./pages/Register";
