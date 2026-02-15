9408fce3f95da55cb6dcc976122f6cd9d7d9d1f2
priyssri923 <chitra.codehub.java@gmail.com>
2026-02-15 21:08:06 +0530

    Update App.jsx

--- my-user-app/src/App.jsx
+++ my-user-app/src/App.jsx
@@ -5,7 +5,7 @@ import {
   Route,
   Navigate,
 } from "react-router-dom";
-import { AuthProvider } from "./context/AuthContext";
+import { AuthProvider } from "./context/Authcontext.jsx";
 import PrivateRoute from "./components/ProtectedRoute.jsx";
 import Login from "./pages/Login";
 import Register from "./pages/Register";
